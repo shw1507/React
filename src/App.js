1b8bfa3be50b86e5e6861e74a54e7d4fa64390bf
신현우 <qnfskqkddlek@naver.com>
2024-02-23 15:24:22 +0900

    와우 3

--- src/App.js
+++ src/App.js
@@ -55,14 +55,4 @@ function Modal() {
   )
 }
 
-function Modal2() {
-  return(
-    <div className='modal'>
-        <h2>제목</h2>
-        <p>날짜</p>
-        <p>상세내용</p> 
-    </div>
-  )
-}
-
 export default App;
